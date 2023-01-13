a5441a143d4dabe4bf9d215fa60b736219e25574
emanRuiz <Emanuel.Ruiz@student.csulb.edu>
2023-01-12 19:48:50 -0800

    overrode onTouchEvent and onDraw
    onDraw states what should happen when you draw
    
    onTouchEvent states when to draw

--- app/src/main/java/com/example/drawingapplication/DrawingView.kt
+++ app/src/main/java/com/example/drawingapplication/DrawingView.kt
@@ -7,6 +7,7 @@ import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.Path
 import android.util.AttributeSet
+import android.view.MotionEvent
 import android.view.View
 
 
@@ -47,11 +48,70 @@ class DrawingView(context: Context, attrs: AttributeSet): View(context, attrs) {
         mBrushSize = 20.toFloat()
     }
 
+    //onSizeChanged - this is called during layout when the size of the view has changed
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        mCanvasBitmap = Bitmap.createBitmap(w,h,Bitmap.Config.ARGB_8888)
+        canvas = Canvas(mCanvasBitmap!!)
+    }
+
+    //If fails change Canvas to Canvas?
+    //This is what should happen when we draw
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        canvas.drawBitmap(mCanvasBitmap!!,0f,0f, mCanvasPaint)
+
+        //make sure draw path is not empty
+        //This deals with what should happen when we draw
+        if(!mDrawPath!!.isEmpty){
+            //how thick the paint
+            mDrawPaint!!.strokeWidth = mDrawPath!!.brushThickness
+            mDrawPaint!!.color = mDrawPath!!.color
+            canvas.drawPath(mDrawPath!!, mDrawPaint!!)
+        }
+    }
+
+    //This is When we Should draw
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        val touchX = event?.x
+        val touchY = event?.y
+
+        when(event?.action)
+        {
+            //when the user touches the screen
+            MotionEvent.ACTION_DOWN -> {
+                //how thick the path
+                mDrawPath!!.color = color
+                mDrawPath!!.brushThickness = mBrushSize
+
+                mDrawPath!!.reset()
+                mDrawPath!!.moveTo(touchX!!, touchY!!)
+            }
+            //when the user moves their finger
+            MotionEvent.ACTION_MOVE-> {
+                mDrawPath!!.lineTo(touchX!!,touchY!!)
+            }
+
+            //when the user lifts their finger
+            MotionEvent.ACTION_UP -> {
+                mDrawPath = CustomPath(color, mBrushSize)
+            }
+            else -> {return false}
+
+
+        }
+        return true
+    }
+
     //This allows the transfer of variables within the class and only within the class
     //Path class encapsulate compound geometric paths consisting lines and curves.
     //It can be drawn of a canvas.
     internal inner class CustomPath(var color: Int, var brushThickness: Float): Path(){
 
     }
+
+    //NOTES
+
+    //
 
 }
